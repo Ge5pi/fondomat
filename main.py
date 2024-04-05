ef8b4bcfa63bfdce5fd7358a0d3b5814b7dc9994
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2024-04-05 20:20:41 +0600

    Start

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect,flash
+
+
+app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'a really really really really long secret key'
+
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
