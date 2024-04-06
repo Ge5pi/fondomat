7723a0f914c8c5f036d685384e7b6f791779e2a0
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2024-04-06 18:37:34 +0600

    created_files

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect,flash
-
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
 app.app_context().push()
@@ -10,6 +9,32 @@ app.config['SECRET_KEY'] = 'a really really really really long secret key'
 @app.route("/")
 def main():
     return render_template("main.html")
+
+
+@app.route("/product")
+def product():
+    return render_template("product.html")
+
+
+@app.route("/company")
+def company():
+    return render_template("company.html")
+
+
+@app.route("/partners")
+def partners():
+    return render_template("partners.html")
+
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+
+@app.route("/news")
+def news():
+    return render_template("news")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
