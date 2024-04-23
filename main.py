8996771f696130fcd4d1e049a52f9b200c03cc96
Ge5pi <157716905+Ge5pi@users.noreply.github.com>
2024-04-23 16:14:23 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
-app.app_context().push()
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'a really really really really long secret key'
 
 
-@app.route("/")
-def main():
-    return render_template("main.html")
-
-@app.route("/base")
-def base():
-    return render_template("base.html")
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
